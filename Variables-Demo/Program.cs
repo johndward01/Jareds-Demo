30cbf466c096af2da1ce038fb0709ad6e1125a20
jdw <johndward01@gmail.com>
2022-01-18 16:20:41 -0600

    init

---
+++ Variables-Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variables_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Decalring a Variable
+            // Syntax: dataType variableName;
+            string x;
+            int y;
+
+            // Initializing a Variable
+            // Syntax: variableName = Value;
+            x = "John";
+            y = 10;
+
+            // Declaring and Initializing 
+            // Syntax: dataType variableName = Value;
+            string fullName = "John Ward";
+            int z = 100;
+
+        }
+    }
+}
